ef3e8ce6e0a47ed80592665fb48e315a53d4be1f
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-03-01 15:27:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Hero from './component/Hero'
+import Navbar from './component/Navbar'
+import About from './component/About'
+import Support from './component/Support'
+import AllInOne from './component/AllInOne'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <Navbar/>
+       <Hero/>
+       <About/>
+       <Support/>
+       <AllInOne/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
