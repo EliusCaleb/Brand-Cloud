a1d46d073cffff35eec34c0072dd3683f44f4f77
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-03-01 15:29:30 +0300

    create allinone page

---
+++ src/component/AllInOne.js
@@ -0,0 +1,154 @@
+import { CheckIcon } from "@heroicons/react/outline";
+import React from "react";
+
+function AllInOne() {
+  return (
+    <div className="w-full my-32">
+      <div className="max-w-[1240px] mx-auto px-4">
+        <h2 className="text-center text-5xl font-bold"> All-In-One Platform</h2>
+        <p className=" text-2xl py-8 text-gray-600 ">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown.
+        </p>
+
+        <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          <div className="flex">
+            <div>
+              <CheckIcon className="w-7 mr-4 text-green-600" />
+            </div>
+
+            <div>
+              <h3 className="font-bold text-lg">Notification</h3>
+              <p className="text-lg">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex">
+            <div>
+              <CheckIcon className="w-7 mr-4 text-green-600" />
+            </div>
+
+            <div>
+              <h3 className="font-bold text-lg">Notification</h3>
+              <p className="text-lg">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex">
+            <div>
+              <CheckIcon className="w-7 mr-4 text-green-600" />
+            </div>
+
+            <div>
+              <h3 className="font-bold text-lg">Notification</h3>
+              <p className="text-lg">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex">
+            <div>
+              <CheckIcon className="w-7 mr-4 text-green-600" />
+            </div>
+
+            <div>
+              <h3 className="font-bold text-lg">Notification</h3>
+              <p className="text-lg">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex">
+            <div>
+              <CheckIcon className="w-7 mr-4 text-green-600" />
+            </div>
+
+            <div>
+              <h3 className="font-bold text-lg">Notification</h3>
+              <p className="text-lg">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text.
+              </p>
+            </div>
+          </div>
+          
+
+          <div className="flex">
+            <div>
+              <CheckIcon className="w-7 mr-4 text-green-600" />
+            </div>
+
+            <div>
+              <h3 className="font-bold text-lg">Notification</h3>
+              <p className="text-lg mr-5">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text.
+              </p>
+            </div>
+          </div>  
+
+
+          <div className="flex">
+            <div>
+              <CheckIcon className="w-7 mr-4 text-green-600" />
+            </div>
+
+            <div>
+              <h3 className="font-bold text-lg">Notification</h3>
+              <p className="text-lg">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text.
+              </p>
+            </div>
+          </div>
+          
+
+          <div className="flex">
+            <div>
+              <CheckIcon className="w-7 mr-4 text-green-600" />
+            </div>
+
+            <div>
+              <h3 className="font-bold text-lg">Notification</h3>
+              <p className="text-lg">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text.
+              </p>
+            </div>
+          </div>
+
+          
+
+
+
+          
+          
+          
+          
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AllInOne;
